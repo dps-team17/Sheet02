e5fb4cb1791b53db4ae36d83a81310010247accc
adanek <andreas.danek-bulius@student.uibk.ac.at>
2016-03-18 09:59:52 +0100

    Basic functionality

--- src/main/java/team17/sheet02/Client.java
+++ src/main/java/team17/sheet02/Client.java
@@ -1,7 +1,13 @@
 package team17.sheet02;
 
-import java.io.*;
-import java.net.*;
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
     public static void main(String[] args) throws IOException {
@@ -16,27 +22,31 @@ public class Client {
         int portNumber = Integer.parseInt(args[1]);
 
         try (
-                Socket kkSocket = new Socket(hostName, portNumber);
-                PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+                Socket socket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(
-                        new InputStreamReader(kkSocket.getInputStream()));
+                        new InputStreamReader(socket.getInputStream()));
         ) {
             BufferedReader stdIn =
                     new BufferedReader(new InputStreamReader(System.in));
             String fromServer;
             String fromUser;
 
-            while ((fromServer = in.readLine()) != null) {
-                System.out.println("Server: " + fromServer);
-                if (fromServer.equals("Bye."))
-                    break;
+            // Wait for welcome
+            fromServer = in.readLine();
+            System.out.println("Server: " + fromServer);
 
-                fromUser = stdIn.readLine();
-                if (fromUser != null) {
-                    System.out.println("Client: " + fromUser);
-                    out.println(fromUser);
-                }
-            }
+            // Send request
+            final JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            objectBuilder.add("operation", "lukas");
+            objectBuilder.add("param1", 10);
+//            objectBuilder.add("param2", 2);
+            out.println(objectBuilder.build().toString());
+
+            // Display answer
+            fromServer = in.readLine();
+            System.out.println("Server: " + fromServer);
+
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
             System.exit(1);
--- src/main/java/team17/sheet02/Protocol.java
+++ src/main/java/team17/sheet02/Protocol.java
@@ -5,6 +5,7 @@ import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 import javax.json.JsonReader;
 import java.io.StringReader;
+import java.security.InvalidParameterException;
 
 public class Protocol {
 
@@ -22,15 +23,19 @@ public class Protocol {
         int result = 0;
         String error = null;
 
-//        final JsonReader reader = Json.createReader(new StringReader(theInput));
-//        final JsonObject jsonObject = reader.readObject();
 
-
-        switch (state){
+        switch (state) {
             case INIT:
                 status = "Welcome. Waiting for requests...";
+                state = WAITING;
                 break;
             case WAITING:
+                try {
+                    status = "Calculation successful";
+                    result = calculate(theInput);
+                } catch (Exception e) {
+                    error = e.getMessage();
+                }
                 break;
             default:
                 error = "Unknown state";
@@ -41,13 +46,63 @@ public class Protocol {
         objectBuilder.add("status", status);
         objectBuilder.add("result", result);
 
-        if(error == null){
+        if (error == null) {
             objectBuilder.addNull("error");
-        }
-        else {
+        } else {
             objectBuilder.add("error", error);
         }
 
         return objectBuilder.build().toString();
+    }
+
+    private int calculate(String jsonString) {
+
+        int result = 0;
+        int a1, a2;
+        final JsonReader reader = Json.createReader(new StringReader(jsonString));
+        final JsonObject jsonObject = reader.readObject();
+
+        String operation = jsonObject.getString("operation");
+
+        switch (operation) {
+            case "+":
+                a1 = jsonObject.getInt("param1");
+                a2 = jsonObject.getInt("param2");
+                result = a1 + a2;
+                break;
+            case "-":
+                a1 = jsonObject.getInt("param1");
+                a2 = jsonObject.getInt("param2");
+                result = a1 - a2;
+                break;
+            case "*":
+                a1 = jsonObject.getInt("param1");
+                a2 = jsonObject.getInt("param2");
+                result = a1 * a2;
+                break;
+            case "lukas":
+                a1 = jsonObject.getInt("param1");
+                result = getLucasNumber(a1);
+                break;
+            default:
+                throw new InvalidParameterException(String.format("Operation %s not supported", operation));
+
+        }
+        return result;
+    }
+
+    int getLucasNumber(int i){
+        if(i < 0){
+            throw new InvalidParameterException("Can not calculate lucas number of negative numbers");
+        }
+        else if( i == 0){
+            return 2;
+        }
+        else if (i == 1){
+            return 1;
+        }
+        else {
+            return getLucasNumber(i-2) + getLucasNumber(i-1);
+        }
     }
 }
