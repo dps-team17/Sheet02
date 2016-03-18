87d4dc1e49c0fdd90c926b35b0cf689e1ee2e948
adanek <andreas.danek-bulius@student.uibk.ac.at>
2016-03-18 22:24:19 +0100

    Transfer

--- src/main/java/team17/sheet02/Protocol.java
+++ src/main/java/team17/sheet02/Protocol.java
@@ -4,7 +4,9 @@ import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 import javax.json.JsonReader;
-import java.io.StringReader;
+import javax.management.OperationsException;
+import java.io.*;
+import java.net.Socket;
 import java.security.InvalidParameterException;
 
 public class Protocol {
@@ -17,6 +19,72 @@ public class Protocol {
     public Protocol() {
         state = 0;
     }
+
+    public static final int PORT = 12345;
+
+    public void request(Socket client, String operation, int... args) throws Exception {
+        try (PrintWriter out =
+                     new PrintWriter(client.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(
+                     new InputStreamReader(client.getInputStream()));) {
+
+            String responseString = null;
+
+            // Open connection
+            responseString = in.readLine();
+            if (!responseString.equals("Waiting")) throw new Exception("Protocol wurde nicht eingehalten");
+
+            //Send request
+            final JsonObjectBuilder builder = Json.createObjectBuilder();
+            builder.add("operation", operation);
+            for (int i = 0; i < args.length; i++) {
+                builder.add(String.format("param%i", i + 1), args[i]);
+            }
+            out.println(builder.build().toString());
+
+            // Wait for responseString
+            responseString = in.readLine();
+            if(responseString == null) throw new Exception("Response is null");
+
+            final JsonReader reader = Json.createReader(new StringReader(responseString));
+            final JsonObject response = reader.readObject();
+
+            String error = response.getString("error");
+            if(error != null) throw new Exception(error);
+
+            int result  = response.getInt("result");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void replay(Socket server) {
+        try (PrintWriter out =
+                     new PrintWriter(server.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(
+                     new InputStreamReader(server.getInputStream()));
+        ) {
+            String inputLine, outputLine;
+
+            // Initiate conversation with client
+            Protocol p = new Protocol();
+            outputLine = p.processInput(null);
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = p.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye."))
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                    + PORT + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+
 
     public String processInput(String theInput) {
         String status = null;
@@ -91,18 +159,15 @@ public class Protocol {
         return result;
     }
 
-    int getLucasNumber(int i){
-        if(i < 0){
+    int getLucasNumber(int i) {
+        if (i < 0) {
             throw new InvalidParameterException("Can not calculate lucas number of negative numbers");
-        }
-        else if( i == 0){
+        } else if (i == 0) {
             return 2;
-        }
-        else if (i == 1){
+        } else if (i == 1) {
             return 1;
-        }
-        else {
-            return getLucasNumber(i-2) + getLucasNumber(i-1);
+        } else {
+            return getLucasNumber(i - 2) + getLucasNumber(i - 1);
         }
     }
 }
--- src/main/java/team17/sheet02/Server.java
+++ src/main/java/team17/sheet02/Server.java
@@ -1,44 +1,22 @@
 package team17.sheet02;
 
-import java.net.*;
-import java.io.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
 
-        if (args.length != 1) {
-            System.err.println("Usage: java KnockKnockServer <port number>");
-            System.exit(1);
-        }
+        Socket clientSocket;
 
-        int portNumber = Integer.parseInt(args[0]);
+        try (ServerSocket serverSocket = new ServerSocket(Protocol.PORT)) {
 
-        try (
-                ServerSocket serverSocket = new ServerSocket(portNumber);
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out =
-                        new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream()));
-        ) {
-
-            String inputLine, outputLine;
-
-            // Initiate conversation with client
-            Protocol p = new Protocol();
-            outputLine = p.processInput(null);
-            out.println(outputLine);
-
-            while ((inputLine = in.readLine()) != null) {
-                outputLine = p.processInput(inputLine);
-                out.println(outputLine);
-                if (outputLine.equals("Bye."))
-                    break;
+            while ((clientSocket = serverSocket.accept()) != null) {
+                Protocol p = new Protocol();
+                p.replay(clientSocket);
             }
-        } catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port "
-                    + portNumber + " or listening for a connection");
-            System.out.println(e.getMessage());
         }
+
+
     }
 }
